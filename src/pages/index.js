3e1ecfbfee337ead3750116a2eda81ad2a62fd32
AAndreazza <alanaandreazza26@hotmail.com>
2023-07-04 14:21:30 -0300

    instagram ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import './global.css'
 import Hero from "../components/hero"
 import About from "../components/about"
+import Instagram from "../components/instagram"
 
 const Index = () => {
     return(
         <div> 
             <Hero />
             <About />
+            <Instagram />
             
         </div>
     )
