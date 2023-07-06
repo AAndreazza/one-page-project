b06cfdf70074b7d17bce2384f603e21a969b4847
AAndreazza <alanaandreazza26@hotmail.com>
2023-07-06 19:08:16 -0300

    iniciando o contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
+require('dotenv').config()
+
 module.exports = {
     plugins:[
         'gatsby-plugin-image',
         'gatsby-plugin-postcss',
         'gatsby-plugin-sharp',
         'gatsby-transformer-sharp',
+        {
+          resolve: 'gatsby-source-contentful',
+          options: {
+           spaceId: process.env.CONTENTFUL_SPACE_ID,
+           accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+          } 
+       },
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
