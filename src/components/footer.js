b32cac6686fd08c6f5b8fa38b1540940411c77c5
AAndreazza <alanaandreazza26@hotmail.com>
2023-07-17 09:39:46 -0300

    ok

--- src/components/footer.js
+++ src/components/footer.js
@@ -56,7 +56,7 @@ const Footer = () => {
                     <label for="email-address" class="sr-only">Email</label>
                     <input type="email" name="email-address" id="email-address" autocomplete="email" required class="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-700 sm:w-64 sm:text-sm sm:leading-6 xl:w-full" placeholder="Digite seu email"/>
                     <div class="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-                        <button type="submit" class="flex w-full items-center justify-center rounded-md bg-sky-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700">Subscribe</button>
+                        <button type="submit" class="flex w-full items-center justify-center rounded-md bg-sky-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700">Increva-se</button>
                     </div>
                 </form>
             </div>
